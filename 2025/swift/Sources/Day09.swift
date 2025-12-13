bf16058e2487a9f06e3b2de9755ee48d7681e68f
Jeff Schmitz <jeffeschmitz@gmail.com>
2025-12-13 09:00:28 -0600

    Day 9: Movie Theater - Part 1 Complete ⭐
    Find largest rectangle using any two red tiles as opposite corners.
    Area calculation includes both corners (inclusive: (|dx|+1) × (|dy|+1)).
    
    Part 1 Answer: 4746238001
    Part 2: Awaiting unlock after Part 1 submission

--- 2025/swift/Sources/Day09.swift
+++ 2025/swift/Sources/Day09.swift
@@ -4,20 +4,47 @@
 // https://adventofcode.com/2025/day/9
 //
 
-
 import AoCTools
 
 final class Day09: AdventOfCodeDay {
-    let title = "Add puzzle title here"
+    let title = "Movie Theater"
+
+    // Red tile coordinates
+    let tiles: [Point]
 
     init(input: String) {
+        // Parse "x,y" coordinates
+        self.tiles = input.lines.compactMap { line -> Point? in
+            let nums = line.integers()
+            guard nums.count == 2 else { return nil }
+            return Point(nums[0], nums[1])
+        }
     }
 
     func part1() async -> Int {
-        return -1
+        // Find largest rectangle using any two tiles as opposite corners
+        // Area includes both corners as tiles (inclusive counting)
+        // width = |x2 - x1| + 1, height = |y2 - y1| + 1
+        var maxArea = 0
+
+        for firstIdx in 0..<tiles.count {
+            for secondIdx in (firstIdx + 1)..<tiles.count {
+                let tile1 = tiles[firstIdx]
+                let tile2 = tiles[secondIdx]
+
+                let width = abs(tile2.x - tile1.x) + 1
+                let height = abs(tile2.y - tile1.y) + 1
+                let area = width * height
+
+                maxArea = max(maxArea, area)
+            }
+        }
+
+        return maxArea
     }
 
     func part2() async -> Int {
+        // Part 2 not yet revealed - need to submit Part 1 answer first
         return -1
     }
 }
