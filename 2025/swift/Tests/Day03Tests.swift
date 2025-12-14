6232dfc9b50472068e15ca102eb159da286bb00e
Jeff Schmitz <jeffeschmitz@gmail.com>
2025-12-14 15:42:17 -0600

    Fix missing test data for Day 3 and Day 4 (#30)
    * Day 4: Add missing test input and expected values
    
    * Day 3: Add missing test input and expected values

--- 2025/swift/Tests/Day03Tests.swift
+++ 2025/swift/Tests/Day03Tests.swift
@@ -9,6 +9,8 @@ import Testing
 @testable import TwelveDaysOfCode
 
 fileprivate let testInput = """
+13579
+97531
 """
 
 @Suite("Day 3 Tests")
@@ -16,7 +18,10 @@ struct Day03Tests {
     @Test("Day 3 Part 1", .tags(.testInput))
     func testDay03_part1() async {
         let day = Day03(input: testInput)
-        await #expect(day.part1() == 0)
+        // Bank [1,3,5,7,9]: max joltage = 79 (7 as tens, 9 as ones)
+        // Bank [9,7,5,3,1]: max joltage = 97 (9 as tens, 7 as ones after)
+        // Total: 79 + 97 = 176
+        await #expect(day.part1() == 176)
     }
 
     @Test("Day 3 Part 1 Solution")
@@ -28,7 +33,9 @@ struct Day03Tests {
     @Test("Day 3 Part 2", .tags(.testInput))
     func testDay03_part2() async {
         let day = Day03(input: testInput)
-        await #expect(day.part2() == 0)
+        // Banks have only 5 digits each (< 12), so all digits kept
+        // 13579 + 97531 = 111110
+        await #expect(day.part2() == 111110)
     }
 
     @Test("Day 3 Part 2 Solution")
--- 2025/swift/Tests/Day04Tests.swift
+++ 2025/swift/Tests/Day04Tests.swift
@@ -9,6 +9,16 @@ import Testing
 @testable import TwelveDaysOfCode
 
 fileprivate let testInput = """
+..@@.@@@@.
+@@@.@.@.@@
+@@@@@.@.@@
+@.@@@@..@.
+@@.@@@@.@@
+.@@@@@@@.@
+.@.@.@.@@@
+@.@@@.@@@@
+.@@@@@@@@.
+@.@.@@@.@.
 """
 
 @Suite("Day 4 Tests")
@@ -16,24 +26,24 @@ struct Day04Tests {
     @Test("Day 4 Part 1", .tags(.testInput))
     func testDay04_part1() async {
         let day = Day04(input: testInput)
-        await #expect(day.part1() == 0)
+        await #expect(day.part1() == 13)
     }
 
     @Test("Day 4 Part 1 Solution")
     func testDay04_part1_solution() async {
         let day = Day04(input: Day04.input)
-        await #expect(day.part1() == 0)
+        await #expect(day.part1() == 1540)
     }
 
     @Test("Day 4 Part 2", .tags(.testInput))
     func testDay04_part2() async {
         let day = Day04(input: testInput)
-        await #expect(day.part2() == 0)
+        await #expect(day.part2() == 43)
     }
 
     @Test("Day 4 Part 2 Solution")
     func testDay04_part2_solution() async {
         let day = Day04(input: Day04.input)
-        await #expect(day.part2() == 0)
+        await #expect(day.part2() == 8972)
     }
 }
